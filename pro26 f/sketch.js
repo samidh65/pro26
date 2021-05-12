b0233a1174a41e58e40cb119b014d97a7f3ccdf0
samidh65 <72860789+samidh65@users.noreply.github.com>
2021-05-12 12:51:37 +0530

    Add files via upload

---
+++ pro26 f/sketch.js
@@ -0,0 +1,21 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+function preload(){
+    
+}
+
+function setup(){
+    engine = Engine.create();
+	world = engine.world;
+    Engine.run(engine); 
+    
+}
+
+function draw(){
+    
+}   
+
